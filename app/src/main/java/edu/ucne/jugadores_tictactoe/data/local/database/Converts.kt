c39b1c81a01ee570acba735fc7469f79db9129e8
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-21 22:42:55 -0400

    Se creó Converts para el uso de la Api

---
+++ app/src/main/java/edu/ucne/jugadores_tictactoe/data/local/database/Converts.kt
@@ -0,0 +1,21 @@
+package edu.ucne.jugadores_tictactoe.data.local.database
+
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class Converters {
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromIntList(value: List<Int?>?): String {
+        return gson.toJson(value)
+    }
+
+    @TypeConverter
+    fun toIntList(value: String): List<Int?> {
+        val listType = object : TypeToken<List<Int?>>() {}.type
+        return gson.fromJson(value, listType)
+    }
+}
