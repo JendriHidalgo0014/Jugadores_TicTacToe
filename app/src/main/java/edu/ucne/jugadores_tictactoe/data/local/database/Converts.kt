e1b39f670dbf1c3f531fe7c3df90fc90921dbef7
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-29 16:06:00 -0400

    Se modifico el converts.

--- app/src/main/java/edu/ucne/jugadores_tictactoe/data/local/database/Converts.kt
+++ app/src/main/java/edu/ucne/jugadores_tictactoe/data/local/database/Converts.kt
@@ -1,21 +1,26 @@
 package edu.ucne.jugadores_tictactoe.data.local.database
 
-
 import androidx.room.TypeConverter
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class Converters {
-    private val gson = Gson()
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val listAdapter = moshi.adapter<List<Int?>>(
+        Types.newParameterizedType(List::class.java, Int::class.javaObjectType)
+    )
 
     @TypeConverter
-    fun fromIntList(value: List<Int?>?): String {
-        return gson.toJson(value)
+    fun fromBoardList(value: List<Int?>?): String? {
+        return value?.let { listAdapter.toJson(it) }
     }
 
     @TypeConverter
-    fun toIntList(value: String): List<Int?> {
-        val listType = object : TypeToken<List<Int?>>() {}.type
-        return gson.fromJson(value, listType)
+    fun toBoardList(value: String?): List<Int?>? {
+        return value?.let { listAdapter.fromJson(it) }
     }
 }
