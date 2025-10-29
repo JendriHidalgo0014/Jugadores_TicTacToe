edbdce6bd28e0e5fb76862c1c519a468c6def4a6
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-29 16:05:05 -0400

    Se agregaron imports y se modificaron algunas funciones.

--- app/src/main/java/edu/ucne/jugadores_tictactoe/di/AppModule.kt
+++ app/src/main/java/edu/ucne/jugadores_tictactoe/di/AppModule.kt
@@ -7,14 +7,20 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.jugadores_tictactoe.data.local.dao.LogroDao
+import edu.ucne.jugadores_tictactoe.data.local.dao.PartidaDao
 import edu.ucne.jugadores_tictactoe.data.local.database.JugadorDatabase
-import edu.ucne.jugadores_tictactoe.data.repository.JugadoresRepositoryImpl
+import edu.ucne.jugadores_tictactoe.data.local.remote.ApiService.JugadorApiService
+import edu.ucne.jugadores_tictactoe.data.local.remote.ApiService.MovimientoApi
+import edu.ucne.jugadores_tictactoe.data.local.remote.DataSource.JugadorRemoteDataSource
+import edu.ucne.jugadores_tictactoe.data.repository.JugadoresApiRepositoryImpl
 import edu.ucne.jugadores_tictactoe.data.repository.LogrosRepositoryImpl
 import edu.ucne.jugadores_tictactoe.data.repository.MovimientoRepositoryImpl
 import edu.ucne.jugadores_tictactoe.data.repository.PartidasRepositoryImpl
 import edu.ucne.jugadores_tictactoe.domain.repository.JugadorRepository
 import edu.ucne.jugadores_tictactoe.domain.repository.MovimientoRepository
 import edu.ucne.jugadores_tictactoe.domain.repository.PartidaRepository
+import edu.ucne.jugadorestictactoe.data.local.Dao.JugadorDao
 import edu.ucne.jugadorestictactoe.domain.repository.LogroRepository
 import javax.inject.Singleton
 
@@ -24,44 +30,57 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideJugadorDb(@ApplicationContext appContext: Context) =
-        Room.databaseBuilder(
+    fun provideJugadorDb(@ApplicationContext appContext: Context): JugadorDatabase {
+        return Room.databaseBuilder(
             appContext,
             JugadorDatabase::class.java,
             "Jugadores.db"
         ).fallbackToDestructiveMigration()
             .build()
+    }
 
     @Provides
-    fun provideJugadorDao(jugadorDatabase: JugadorDatabase) = jugadorDatabase.JugadorDao()
+    fun provideJugadorDao(jugadorDatabase: JugadorDatabase): JugadorDao =
+        jugadorDatabase.jugadorDao()
 
     @Provides
-    fun provideLogroDao(logroDatabase: JugadorDatabase) = logroDatabase.LogroDao()
+    fun provideLogroDao(jugadorDatabase: JugadorDatabase): LogroDao =
+        jugadorDatabase.logroDao()
 
     @Provides
-    fun providePartidaDao(partidaDatabase: JugadorDatabase) = partidaDatabase.PartidaDao()
+    fun providePartidaDao(jugadorDatabase: JugadorDatabase): PartidaDao =
+        jugadorDatabase.partidaDao()
+
+    @Provides
+    @Singleton
+    fun provideJugadorRemoteDataSource(
+        jugadorApi: JugadorApiService
+    ): JugadorRemoteDataSource = JugadorRemoteDataSource(jugadorApi)
 
     @Provides
     @Singleton
     fun provideJugadorRepository(
-        jugadorDao: edu.ucne.jugadores_tictactoe.data.local.dao.JugadorDao
-    ): JugadorRepository = JugadoresRepositoryImpl(jugadorDao)
+        localDataSource: JugadorDao,
+        remoteDataSource: JugadorRemoteDataSource
+    ): JugadorRepository {
+        return JugadoresApiRepositoryImpl(localDataSource, remoteDataSource)
+    }
 
     @Provides
     @Singleton
     fun provideLogroRepository(
-        logroDao: edu.ucne.jugadores_tictactoe.data.local.dao.LogroDao
+        logroDao: LogroDao
     ): LogroRepository = LogrosRepositoryImpl(logroDao)
 
     @Provides
     @Singleton
     fun providePartidaRepository(
-        partidaDao: edu.ucne.jugadores_tictactoe.data.local.dao.PartidaDao
+        partidaDao: PartidaDao
     ): PartidaRepository = PartidasRepositoryImpl(partidaDao)
 
     @Provides
     @Singleton
     fun provideMovimientoRepository(
-        movimientoApi: edu.ucne.jugadores_tictactoe.data.local.remote.PartidaApi.MovimientoApi
+        movimientoApi: MovimientoApi
     ): MovimientoRepository = MovimientoRepositoryImpl(movimientoApi)
 }
